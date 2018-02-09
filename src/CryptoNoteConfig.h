082b9e280e59ca2e51cda1e9a3a26c2d70b108ef
cis <sachin.s@cisinlabs.com>
2018-02-09 17:31:08 +0530

    Add Genesis Block in configuration File

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -76,7 +76,7 @@ const char     MINER_CONFIG_FILE_NAME[]                      = "miner_conf.json"
 
 //TODO Put here the name of your currency
 const char     CRYPTONOTE_NAME[]                             = "kreditX";
-const char     GENESIS_COINBASE_TX_HEX[]                     = "";
+const char     GENESIS_COINBASE_TX_HEX[]                     = "013c01ff0001ffffffffffff0f029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd08807121012601b25a29ff6c7c5edc7d3382c99700dc7ee082709715c67628ad506b0aca47";
 
 const uint8_t  CURRENT_TRANSACTION_VERSION                   =  1;
 const uint8_t  BLOCK_MAJOR_VERSION_1                         =  1;
